1e4f762c7ac6f45a18447b4c680461bd8cfa0616
swayam suryavanshi <69519733+swwayam@users.noreply.github.com>
2022-08-15 16:32:28 +0530

    Final

--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Player {
+    // Class for Player Info
     Scanner input = new Scanner(System.in);
     String name;
 
-    String playerInfo(int playerNumber) {
+    void playerInfo(int playerNumber) {
         System.out.println("Enter the name of player " + playerNumber);
         name = input.next();
-        return name;
+
     }
 
 }
@@ -17,18 +18,19 @@ class Umpire {
     Scanner input = new Scanner(System.in);
     Random rand = new Random();
     int generatedRandomNumber;
-    int guess;
+    int playerGuess;
     int numberOfWinners = 0;
 
-    int generateRandomNumber() {
+    void generateRandomNumber() {
+        // Everytime the game will Generate a random number to be fare in the game
         generatedRandomNumber = rand.nextInt(11);
-        return generatedRandomNumber;
+
     }
 
     int numberGussedByPlayer() {
-        guess = input.nextInt();
-        if (guess <= 10 && guess >= 0) {
-            return guess;
+        playerGuess = input.nextInt();
+        if (playerGuess <= 10 && playerGuess >= 0) {
+            return 0;
         } else {
             System.out.println("Please re-enter the number in range 0-11");
             numberGussedByPlayer();
@@ -37,10 +39,10 @@ class Umpire {
     }
 
     String compare() {
-        if (generatedRandomNumber == guess) {
+        if (generatedRandomNumber == playerGuess) {
             numberOfWinners++;
             return "Youuu winn!!! Hurrayyyyy!!!";
-        } else if (guess < generatedRandomNumber) {
+        } else if (playerGuess < generatedRandomNumber) {
             return "your guess was on the lower side!";
         } else {
             return "your guess was on the higher side!";
@@ -50,7 +52,6 @@ class Umpire {
 
 public class Main {
     public static void main(String[] args) {
-        // Generating a random number to be fare in the game
         Scanner input = new Scanner(System.in);
 
         // Umpire class with compare function
@@ -65,7 +66,7 @@ public class Main {
         // Creating a player and checking for the condition
         for (int playerNumber = 1; playerNumber <= TotalNumOfPlayers; playerNumber++) {
             // Generating a random number everytime for seperate player and storing it in umpire class
-            int randomNumber = umpire.generateRandomNumber();
+            umpire.generateRandomNumber();
 
             // Making a player and getting the information from the player
             Player player = new Player();
@@ -73,20 +74,20 @@ public class Main {
 
             // Calling the umpire to listen to the number guessed by the player
             System.out.println("Please enter your guess in range of 0-11");
-            int randomNumberByPlayer = umpire.numberGussedByPlayer();
+            umpire.numberGussedByPlayer();
 
             // Asking umpire to check if the number is correct
             String umpireAnswer = umpire.compare();
 
             // Printing out the results of individual players
             System.out.println();
-            System.out.println("The guessed number was " + randomNumberByPlayer);
-            System.out.println("The Number generated was " + randomNumber);
+            System.out.println("The guessed number was " + umpire.playerGuess);
+            System.out.println("The Number generated was " + umpire.generatedRandomNumber);
             System.out.println();
             System.out.println(player.name + " " + umpireAnswer);
             System.out.println();
             if (!(playerNumber == TotalNumOfPlayers)) {
-                System.out.println("Generating another random number for player number " + (int) (playerNumber + 1));
+                System.out.println("Generating another random number for player number " +  (playerNumber + 1));
             } else {
                 System.out.println("Thanks for playing! " + TotalNumOfPlayers + " Players Played today's game and " + umpire.numberOfWinners + " Won today's game");
                 System.out.println();
